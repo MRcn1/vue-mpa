a6c67984c7db8288a3f352088dcec2f4ad7e1610
chenwen <bini159@163.com>
2020-06-04 14:56:19 +0800

    vue 多页面 模板

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ let pageMethod = require('./util/getPages.js');
 pages = pageMethod.pages();
 module.exports = {
     pages,
+    lintOnSave:false,
     publicPath: process.env.NODE_ENV === 'production'
     ? '/dist/'
     : '/',
